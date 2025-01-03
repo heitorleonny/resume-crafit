eb2e49688b5d604c228aadd0d9d1e1a29aa92eb1
Heitor Leonny <108541219+heitorleonny@users.noreply.github.com>
2025-01-03 19:33:37 -0300

    Initial commit

---
+++ src/components/pages/dashboard/user-dropdown.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { LogOut, SquareUser } from "lucide-react";
+
+export const UserDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant="ghost" className="w-full gap-2 justify-start px-2">
+          <Avatar className="w-7 h-7 block">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>HL</AvatarFallback>
+          </Avatar>
+          <p>Heitor Leonny</p>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="center"
+        className="w-[var(--radix-dropdown-menu-trigger-width)]"
+      >
+        <Link passHref href="/dashboard/account">
+          <DropdownMenuItem className="gap-2">
+            <SquareUser size={16} />
+            Configurações de Conta
+          </DropdownMenuItem>
+        </Link>
+
+        <DropdownMenuItem className="gap-2 text-red-500">
+          <LogOut size={16} />
+          Sair
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
